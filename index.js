8b954e961cf6d12fd51a43779cc5aa02feda9001
ayden <aydendiel@gmail.com>
2025-06-20 20:22:42 -0400

    feat: added crt-style curvature to render

--- index.js
+++ index.js
@@ -471,14 +471,24 @@ uniform float u_pointSize;
 
 varying vec4 clipPosition;
 
+// from https://www.shadertoy.com/view/XtlSD7
+vec2 CRTCurveUV(vec2 uv){
+    uv = uv * 2.0;
+    vec2 offset = abs( uv.yx ) / vec2( 6.0, 4.0 );
+    uv = uv - uv * offset * offset * offset;
+    uv = uv * 0.5;
+    return uv;
+}
+
 void main() {
   float offset = a_position.z;
   gl_Position = vec4(
     (a_position.x / (${WIDTH}.0) - 0.5) * 2.0 + offset / 100.0,
     ((${HEIGHT}.0 - 1.0 - a_position.y) / ${HEIGHT}.0 - 0.5) * 2.0 + offset / 100.0,
     0.0, 1.0);
+  gl_Position = vec4(CRTCurveUV(gl_Position.xy), gl_Position.z, gl_Position.w);
   gl_PointSize = u_pointSize;
-  clipPosition = u_pointSize;
+  clipPosition = gl_Position;
 }
 `;
 
@@ -491,7 +501,19 @@ float rand(vec2 co){
     return sin(dot(co, vec2(12.9898, 78.233))) / 8.0;
 }
 
+// from https://www.shadertoy.com/view/XtlSD7
+vec2 CRTCurveUV(vec2 uv){
+    uv = uv * 2.0 - 1.0;
+    vec2 offset = abs( uv.yx ) / vec2( 6.0, 4.0 );
+    uv = uv + uv * offset * offset;
+    uv = uv * 0.5 + 0.5;
+    return uv;
+}
+
 void main() {
+
+  vec2 uv = clipPosition.xy / vec2(${WIDTH}.0, ${HEIGHT}.0);
+
   float rand = rand(gl_PointCoord);
   vec2 coord = gl_PointCoord - vec2(0.5);
   float dist = length(coord);
